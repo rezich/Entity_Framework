3346aa92936bfec644b274893bf40734b2656084
Adam Rezich <adam@rezich.com>
2021-11-02 16:50:41 -0600

    split examples into simple and game

--- examples/game/build.bat
+++ examples/game/build.bat
@@ -2,4 +2,4 @@
 REM this is just to import the Entity_Storage module; you'll probably have the
 REM module stored elsewhere for your project
 
-jai build.jai -import_dir ../../
+jai build.jai -import_dir ../../../
---
+++ examples/simple/build.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+REM this is just to import the Entity_Storage module; you'll probably have the
+REM module stored elsewhere for your project
+
+jai build.jai -import_dir ../../../
